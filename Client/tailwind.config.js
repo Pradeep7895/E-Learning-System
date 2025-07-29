e74e658280234787439a83349e8e111661ee5189
gla1248-pradeep <pradeep.chaudhary_cs22@gla.ac.in>
2025-07-30 00:22:47 +0530

    first commit

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -32,6 +32,13 @@ export default {
       borderRadius: {
         lg: "var(--radius)",
       },
+      zIndex: {
+        60: "60",
+        70: "70",
+        80: "80",
+        90: "90",
+        100: "100",
+      },
     },
   },
   plugins: [animate],
